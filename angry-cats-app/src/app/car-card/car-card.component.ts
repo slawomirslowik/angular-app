82be60b5409375e9682fe00309f48f90cd9eb732
Slawomir Slowik <Slawomir.Slowik@sapiens.com>
2024-02-02 17:32:55 +0100

    changed to use component with direct variables

---
+++ angry-cats-app/src/app/car-card/car-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-car-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-card.component.html',
+  styleUrl: './car-card.component.scss'
+})
+
+export class CarCardComponent {
+
+    @Input() brand! : string;
+
+    @Input() model! : string;
+
+    @Input() imgSrc! : string;
+}
